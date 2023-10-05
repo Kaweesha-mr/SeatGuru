d06abe7d497b569218c3c11fee5e3ee297b008ab
kaweesha-mr <kaweesha.mr@gmail.com>
2023-10-05 14:03:04 +0530

    Password hashing method added

--- SeatGuru/src/main/java/com/example/seatguru/Register.java
+++ SeatGuru/src/main/java/com/example/seatguru/Register.java
@@ -1,4 +1,28 @@
 package com.example.seatguru;
 
-public class Register {
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet ("/register")
+public class Register extends HttpServlet {
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String NIC = req.getParameter("NIC");
+        String FName = req.getParameter("FName");
+        String LName = req.getParameter("LName");
+        String Email = req.getParameter("Email");
+        String Password = req.getParameter("Password");
+        Password = passwordHash.doHashing(Password);
+
+
+
+
+
+    }
 }
---
+++ SeatGuru/src/main/java/com/example/seatguru/passwordHash.java
@@ -0,0 +1,23 @@
+package com.example.seatguru;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class passwordHash {
+
+    public static String doHashing(String Password){
+        try{
+            MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+            messageDigest.update(Password.getBytes());
+            byte[] resultByteArray = messageDigest.digest();
+            StringBuilder sb = new StringBuilder();
+            for (byte b : resultByteArray) {
+                sb.append(String.format("%02x", b));
+            }
+            return sb.toString();
+
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
